938714311dfe9bead62ebd656c8bacad52ada132
Tim Hu <tim@htwt.(none)>
2017-10-15 00:28:02 +0800

    add constructor

--- 7_class/7_prac/prac_7.7.cpp
+++ 7_class/7_prac/prac_7.7.cpp
@@ -1,26 +1,33 @@
 //rewrite the transaction-processing program
 #include <iostream>
-using std::cout;using std::cin; using std::endl;
+#include "Sales_data.h"
+//using std::cout;using std::cin; using std::endl;
+using namespace std;
 
 int main()
 {
-    cout << "please input ISBN, units_sold, sellingprice, saleprice" << 
-endl;
+    cout << "Hello,world" << endl;
+    cout << "please input ISBN, units_sold, sellingprice, saleprice" 
+<< endl;
     Sales_data total;
-    if (cin >> total)
+    if (read(cin, total))
+    //cout << "finish cin" << endl;
+    //if (cin >> total)
     {
         Sales_data trans;                     //save next transaction 
-        while (cin >> trans)
+        while (read(cin,trans))
         {
             if (total.isbn() == trans.isbn())
                 total.combine(trans);         //updata total transaction
             else
             {
-                cout << total << endl;        //??
+                //cout << "total" << endl;        //??
+                print(cout, total);
                 total = trans;                //deal with next book
             }
         }
-        cout << total << endl;                //print the last book msg
+        print (cout, total);
+        //cout << "total" << endl;            //print the last book msg
     }
     else
     {
@@ -29,3 +36,4 @@ endl;
     }
     return 0;
 }
+
