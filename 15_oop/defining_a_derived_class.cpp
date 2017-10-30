dd090bb7f0f66d564e08fa9afe9851991e4465c8
timtingwei <timtingwei@hotmail.com>
2017-10-30 12:06:40 +0800

    add other test for defining a derived class

--- 15_oop/defining_a_derived_class.cpp
+++ 15_oop/defining_a_derived_class.cpp
@@ -22,9 +22,12 @@ protected:
 class Bulk_quote : public Quote {  //Bulk_quote inherits from Quote
 public:
   Bulk_quote() = default;
-  Bulk_quote(const std::string&, double, std::size_t,double);
+  Bulk_quote(const std::string& book, double p, std::size_t qty,double disc):
+    Quote(book, p), min_qty(qty), discount(disc){ }
   
   double net_price(std::size_t) const override;
+
+  virtual ~Bulk_quote() = default;
 private:
   std::size_t min_qty = 0;        //minimun purchase for the discount to apply
   double discount = 0.0;          //fractional discount to apply
@@ -41,11 +44,59 @@ double print_total(std::ostream &os,
   return ret;
 }
 
+double Bulk_quote::net_price(size_t cnt) const
+{
+  if (cnt >= min_qty)
+    return cnt * (1-discount) * price;
+  else
+    return cnt * price;
+}
+
+/*
+class Base{
+public:
+  static void statmem();
+};
+
+class Derived : public Base {
+  void f(const Derived&);
+};
+
+void Derived::f(const Derived &derived_obj)
+{
+  Base::statmem();        //ok:Base defines statmem
+  Derived::statmem();     //ok:Derived inherits statmem
+  //ok: derived objects can be used to access static from base
+  derived_obj.statmem();  //accessed through a Derived object
+  statmem();              //accessed through this object
+  //::stamem();
+}
+*/
+
+//Classes used to a Base Class
+class Base {/*...*/};
+class D1: public Base {/*...*/};
+class D2: public D1 {/*...*/};
+
+//Preventing Inheritance
+class NoDerived final {/*...*/};      //NoDerived can't be a base class
+class Base {/*...*/};
+class Last final : Base {/*...*/};    //Last cannot be a base class
+//class Bad : NoDerived {/*...*/};      //error: NoDerived is final | error: cannot derive from ‘final’ base ‘NoDerived’ in derived type ‘Bad’
+//class Bad2 : Last {/*...*/};          //error:Last if final
+
 int main()
 {
-  //Quote basic;
-  //Bulk_quote bulk;
-  //print_total(std::cout,basic,20);  //cakk Quote version of net_price
-  //print_total(std::cout,bulk,20);   //call Bulk_quote version of net_price
+  Quote basic;
+  Bulk_quote bulk;
+  print_total(std::cout,basic,20);  //cakk Quote version of net_price
+  print_total(std::cout,bulk,20);   //call Bulk_quote version of net_price
+
+  //Quote item;
+  //bulk_quote bulk;
+  //Quote *p = &item;
+  //p = &bulk;
+  //Quote &r = bulk;
+
   return 0;
 }
