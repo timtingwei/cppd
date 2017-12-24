316dab4bfa6389447d2208c6450a908f643682d3
timtingwei <timtingwei@hotmail.com>
2017-12-24 11:11:39 +0800

    remove Primer files in this context

--- 7_class/7_prac/prac_7.16.cpp
+++
@@ -1,4 +0,0 @@
-
-
-//after public:constructor and member functions
-//after private:data members and functions of implementation
--- 7_class/7_prac/prac_7.17.cpp
+++
@@ -1,3 +0,0 @@
-
-//difference between class and struct
-//define a class is the default access level
