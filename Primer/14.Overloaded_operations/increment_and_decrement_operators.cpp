103a50611b93c95c1be3ec20da0674df70b240e8
timtingwei <timtingwei@hotmail.com>
2018-01-10 10:51:52 +0800

    modified

--- Primer/14.Overloaded_operations/increment_and_decrement_operators.cpp
+++ Primer/14.Overloaded_operations/increment_and_decrement_operators.cpp
@@ -31,12 +31,9 @@ StrBlobPtr& StrBlobPtr::operator++() {
 
 
 StrBlobPtr StrBlobPtr::operator++(int) {    // 后置递增不返回引用类型
-  std::cout << "calling back increment ....."
-            << "now curr = " << curr << std::endl;
   StrBlobPtr s = *this;     // 保存当前值备份
-  std::cout << "s.curr = " << s.curr << std::endl;
   // ++curr;
-  ++*this;
+  ++*this;                   // 后置递增以前置递增为基础 
   return s;                  // 后置递增, 返回递增前的值
 }
 
@@ -54,17 +51,37 @@ int main() {
   std::size_t c = 5;
   StrBlobPtr s(c);
   s.curr_i = 5;
+  // -- ------ test forward increment --------
+  std::cout << "-- ------ test forward increment -------- --" << std::endl;
   s.operator++();   // 显式调用前置递增
   s.print_curr();
   ++s;              // 重载前置递增后的调用
   s.print_curr();
-  // -- ------ test back increment --------
-  std::cout << "-- ------ test back increment -------- --" << std::endl;
-  // (++s).print_curr();
-  // (s++).print_curr();
-  std::cout << "(s++).curr_i = " << (s++).curr_i << std::endl;
-  // std::cout << "s++.cerr"(s++).print_curr();
-  // s.print_curr();
   
+  // -- ------ test backward increment --------
+  std::cout << "-- ------ test back increment -------- --" << std::endl;
+  s.operator++(1).print_curr();   // 显式调用后置递增
+  (++s).print_curr();             // 前置递增
+  (s++).print_curr();             // 后置递增
+  std::cout << "(s++).curr_i = " << (s++).curr_i << std::endl;
+  s.print_curr();
   return 0;
 }
+
+/* $ ./a.out
+-- ------ test forward increment -------- --
+curr = 6
+curr_i = 6
+curr = 7
+curr_i = 7
+-- ------ test back increment -------- --
+curr = 7
+curr_i = 7
+curr = 9
+curr_i = 9
+curr = 9
+curr_i = 9
+(s++).curr_i = 10
+curr = 11
+curr_i = 11
+ */
