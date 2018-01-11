e70e49f0f596efcfc8b7a3df1200faf94409fdb0
timtingwei <timtingwei@hotmail.com>
2018-01-11 12:04:44 +0800

    difine a static member and increment it

---
+++ tmp/static.cpp
@@ -0,0 +1,52 @@
+/*  ------------------------------
+    Copyright <2018> [Tim Hu]
+    email: timtingwei@hotamail.com    
+   ------------------------------
+   File:vector_disordered.cpp
+   Date:Thu Jan 11 11:30:45 CST 2018
+   -----------------------------
+*/
+
+/*
+类内static类型如何声明?
+在不同实例中被多次调用改变后的情况?
+ */
+#include <iostream>
+
+class MyStatic {
+  // static int i_stc = 5;     // 只能通过定义初始化
+  
+ public:
+  static int i_stc;
+  static void set_static(int i) {i_stc = i;}
+  // MyStatic() {set_static(0);}
+  void increment() {i_stc += 1;}
+  // virtual operator()() {i_stc += 1;}    // 函数对象改变static变量的值
+};
+
+int MyStatic::i_stc = 0;
+// MyStatic::set_static(0);   // 初始化静态成员
+
+void f(){
+  
+  MyStatic c0;
+  c0.increment();
+  std::cout << "c0 = " << c0.i_stc << std::endl;
+  MyStatic c1;
+  c1.increment();
+  std::cout << "c1 = " << c1.i_stc << std::endl;
+}
+
+void f1() {
+  MyStatic c2;
+  c2.increment();
+  std::cout << "c2 = " << c2.i_stc << std::endl;
+}
+
+
+int main() {
+  // ..
+  f();
+  f1();
+  return 0;
+}
