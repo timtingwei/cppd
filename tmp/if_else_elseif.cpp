f01d80aac7180374e119e9c4d0be18458d7e7c5f
timtingwei <timtingwei@hotmail.com>
2018-01-12 22:50:39 +0800

    test if...else and if..else  else..if

---
+++ tmp/if_else_elseif.cpp
@@ -0,0 +1,32 @@
+/*  ------------------------------
+    Copyright <2018> [Tim Hu]
+    email: timtingwei@hotamail.com    
+   ------------------------------
+   File:if_else_elseif.cpp
+   Date:Fri Jan 12 22:42:07 CST 2018
+   -----------------------------
+*/
+
+#include <iostream>
+
+/*
+  if...else 和 if...else if的区别
+ */
+
+void f_else() {
+  int i = 5;
+  if (i == 5) {std::cout << "i = 5" << std::endl;
+  } else {std::cout << "else if" << std::endl;}
+}
+
+void f_else_if() {
+  int i = 4;
+  if (i == 5) std::cout << "i = 5" << std::endl;
+  else if (i < 5) std::cout << "i < 5" << std::endl;
+  else if (i > 5) std::cout << "i > 5" << std::endl;
+}
+
+int main() {
+  // f();
+  f_else_if();
+}
