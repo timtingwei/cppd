7bdfc971cde5ad4eda9b2832102ffaf93c01dba4
timtingwei <timtingwei@hotmail.com>
2018-01-16 22:35:05 +0800

    test  local value deconstructor order

---
+++ CPL/10.Classes/10.4.Objects/class_local.cpp
@@ -0,0 +1,47 @@
+/*  ------------------------------
+    Copyright <2018> [Tim Hu]
+    email: timtingwei@hotamail.com    
+   ------------------------------
+   File:class_local.cpp
+   Date:Tue Jan 16 22:27:37 CST 2018
+   -----------------------------
+*/
+
+#include <iostream>
+
+class Name {
+  const char* s;
+};
+
+class Table {
+  Name* p;
+  size_t sz;
+ public:
+  explicit Table(size_t s = 15) {                 // constructor
+    p = new Name[sz = s];
+    for (int i = 0; i < sz; i++) {
+      std::cout << "i = " << i << std::endl;
+    }
+  }
+  ~Table() {                                      // deconstructor
+    delete[] p;
+    std::cout << "sz = " << sz << std::endl;
+  }
+  Name* lookup(const char*);
+  bool insert(Name*);
+};
+
+void f(int i) {
+  Table aa(1);
+  Table bb(2);
+  if (i > 0) {
+    Table cc(3);
+  }
+  Table dd(3);
+}
+
+int main() {
+  f();
+
+  return 0;
+}
