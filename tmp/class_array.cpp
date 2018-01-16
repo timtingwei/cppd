2f27dc67c44cbc1c9c66e6b5b68121294e8548ae
timtingwei <timtingwei@hotmail.com>
2018-01-16 23:58:20 +0800

    test class pointer

---
+++ tmp/class_array.cpp
@@ -0,0 +1,52 @@
+/*  ------------------------------
+    Copyright <2018> [Tim Hu]
+    email: timtingwei@hotamail.com    
+   ------------------------------
+   File:class_array.cpp
+   Date:Tue Jan 16 23:50:38 CST 2018
+   -----------------------------
+*/
+
+#include <iostream>
+
+class Name {
+  const char* s;
+};
+
+void test_array() {
+  Name* t;
+  t[0];
+}
+
+/*
+class Table {
+  Name* p;
+  size_t sz;
+ public:
+  explicit Table(size_t s = 15) {                 // constructor
+    p = new Name[sz = s];
+    for (int i = 0; i < sz; i++) {
+      // std::cout << "in Table() i = " << i << std::endl;
+    }
+    std::cout << "in Table() sz = " << sz << std::endl;
+  }
+  ~Table() {                                      // deconstructor
+    delete[] p;
+    std::cout << "in ~Table() sz = " << sz << std::endl;
+  }
+  Name* lookup(const char*);
+  bool insert(Name*);
+
+  // 将复制定义清楚
+  Table(const Table&);              // 复制构造函数
+  Table& operator=(const Table&);   // 复制赋值
+};
+*/
+
+int main() {
+  // h();
+  // f();
+  test_array();
+
+  return 0;
+}
