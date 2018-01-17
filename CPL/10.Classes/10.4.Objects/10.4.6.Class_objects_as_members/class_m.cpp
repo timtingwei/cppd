5a0578e4bcadf56dd5aea39512941b3fdd3376ad
timtingwei <timtingwei@hotmail.com>
2018-01-17 22:46:22 +0800

    construct Club class

---
+++ CPL/10.Classes/10.4.Objects/10.4.6.Class_objects_as_members/class_m.cpp
@@ -0,0 +1,79 @@
+/*  ------------------------------
+    Copyright <2018> [Tim Hu]
+    email: timtingwei@hotamail.com    
+   ------------------------------
+   File:class_m.cpp
+   Date:Wed Jan 17 22:41:36 CST 2018
+   -----------------------------
+*/
+
+#include <iostream>
+#include <string.h>
+
+class Name {
+  const char* s;
+};
+
+class Table {
+  Name* p;
+  size_t sz;
+ public:
+  explicit Table(size_t s = 15) {                 // constructor
+    p = new Name[sz = s];
+    for (int i = 0; i < sz; i++) {
+      // std::cout << "in Table() i = " << i << std::endl;
+    }
+    std::cout << "in Table() sz = " << sz << std::endl;
+  }
+  ~Table() {                                      // deconstructor
+    delete[] p;
+    std::cout << "in ~Table() sz = " << sz << std::endl;
+  }
+  Name* lookup(const char*);
+  bool insert(Name*);
+
+  // 将复制定义清楚
+  Table(const Table&);              // 复制构造函数
+  Table& operator=(const Table&);   // 复制赋值
+};
+
+Table::Table(const Table& t) {
+  p = new Name[sz = t.sz];
+  for (int i = 0; i < sz; i++) {
+    p[i] = t.p[i];
+  }
+}
+
+Table& Table::operator=(const Table& t) {
+  if (this != &t) {         // 防止自复制
+    delete[] p;
+    p = new Name[sz = t.sz];
+    for (int i = 0; i < sz; i++) {p[i] = t.p[i];}
+  }
+  return *this;
+}
+
+class Date{};
+
+
+// 定义小型有关信息组织的类
+class Club {
+  std::string name;
+  Table members;
+  Table officers;
+  Date founded;
+  // ...
+  Club(const std::string& n, Date fd);
+};
+
+void h() {
+  
+}
+
+
+int main() {
+  h();
+  // f();
+
+  return 0;
+}
