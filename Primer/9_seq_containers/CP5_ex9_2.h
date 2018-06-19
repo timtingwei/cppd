6d8fa564817eedfb6015867eedbe0db7911e5978
timtingwei <timtingwei@hotmail.com>
2018-06-20 03:13:09 +0800

    ex9

---
+++ Primer/9_seq_containers/CP5_ex9_2.h
@@ -0,0 +1,20 @@
+/*  ------------------------------
+    Copyright <2018> [Tim Hu]
+    email: timtingwei@hotamail.com    
+   ------------------------------
+   File:CP5_ex9_2.h
+   Date:Wed Jun 20 02:08:26 CST 2018
+   -----------------------------
+*/
+
+#ifndef CP5_ex9_2_h
+#define CP5_ex9_2_h
+
+#include <iostream>
+#include <list>
+#include <deque>
+
+std::list<std::deque<int> > myLst;
+
+#endif
+
