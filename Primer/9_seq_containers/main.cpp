7c0135798c108d6347f63cd835b9cca74ce8553c
timtingwei <timtingwei@hotmail.com>
2018-06-21 21:01:14 +0800

    test push_front, insert, push_back

--- Primer/9_seq_containers/main.cpp
+++ Primer/9_seq_containers/main.cpp
@@ -10,7 +10,6 @@
 #include <vector>
 #include <list>
 #include <string>
-#include "CP5_ex9_16.h"
 
 using std::vector; using std::list;
 using std::string;
@@ -18,7 +17,42 @@ using std::string;
 int main() {
   
   std::vector<int> ivec = {0, 1, 2, 3};
-  std::list<int> ilist = {0, 1, 2, 3};
-  std::cout << v_l_compare(ivec, ilist) << std::endl;
+  int i = 5;
+  ivec.push_back(i);
+  
+  string s = "tim";
+  s.push_back('H');
+  std::cout << "s = " << s << std::endl;
+  s += 'U';
+  std::cout << "s = " << s << std::endl;
+
+  std::list<int> ilist;
+  for (size_t ix = 0; ix != 4; ix++)
+    ilist.push_front(ix);  // equal to : ilist.insert(ilist.begin(), ix);
+
+  for (auto iter = ilist.cbegin(); iter != ilist.cend(); iter++)
+    std::cout << *iter << std::endl;
+
+  auto iter = ilist.end();
+  ilist.insert(iter, 5);
+  
+  for (auto iter = ilist.cbegin(); iter != ilist.cend(); iter++)
+    std::cout << *iter << std::endl;
+
+  ilist.insert(ilist.end(), {11, 22, 33, 44});
+  for (auto iter = ilist.cbegin(); iter != ilist.cend(); iter++)
+    std::cout << *iter << std::endl;
+
+  ilist.insert(ilist.begin(), ilist.begin(), ilist.end());
+  for (auto iter = ilist.cbegin(); iter != ilist.cend(); iter++)
+    std::cout << *iter << std::endl;
+
+  list<string> lst;
+  string word;
+  auto iter_s = lst.begin();
+  while (std::cin >> word) {
+    iter_s = lst.insert(iter_s, word);
+    std::cout << *iter_s << std::endl;
+  }
   return 0;
 }
