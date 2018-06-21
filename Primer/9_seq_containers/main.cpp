2f78edc479cc1a024fd333c591c505858af09407
timtingwei <timtingwei@hotmail.com>
2018-06-21 16:26:23 +0800

    finish 9.2

--- Primer/9_seq_containers/main.cpp
+++ Primer/9_seq_containers/main.cpp
@@ -10,30 +10,15 @@
 #include <vector>
 #include <list>
 #include <string>
+#include "CP5_ex9_16.h"
 
 using std::vector; using std::list;
 using std::string;
 
 int main() {
-  list<string> names;
-  vector<const char*> oldstyles = {"tim", "cooper"};
-
-  // names = oldstyles;
-  names.assign(oldstyles.cbegin(), oldstyles.cend());
-
-  list<int> ilist(1);  // an empty element
-  ilist.assign(10, -1);
-
-  for (auto iter = ilist.cbegin(); iter != ilist.cend(); iter++)
-    std::cout << *iter << std::endl;
-
-  // equal to
-  list<int> ilist1(1);  // an empty element
-  ilist1.clear();
-  ilist1.insert(ilist1.begin(), 10, -5);
-
-  for (auto iter = ilist1.cbegin(); iter != ilist1.cend(); iter++)
-    std::cout << *iter << std::endl;
   
+  std::vector<int> ivec = {0, 1, 2, 3};
+  std::list<int> ilist = {0, 1, 2, 3};
+  std::cout << v_l_compare(ivec, ilist) << std::endl;
   return 0;
 }
