b9175029ecb60ee5b732594bec3f83d27209f087
timtingwei <timtingwei@hotmail.com>
2018-06-21 11:18:47 +0800

    test std::array initialize

--- Primer/9_seq_containers/main.cpp
+++ Primer/9_seq_containers/main.cpp
@@ -7,23 +7,16 @@
    -----------------------------
 */
 #include <iostream>
-#include <vector>
-#include <list>
-#include <forward_list>
-#include <deque>
-#include <string>
-
-// using std::vector; using std::list; using std::deque; using std::forward_list;
-// using std::string;
+#include <array>
 
 int main() {
-  std::vector<int> ivec(10, -1);
-  std::list<std::string> svec(5, "tim");
-  std::forward_list<int> fivec(10);
-  std::deque<std::string> dsvec(10);
+  std::array<int, 10> iarr1;
+  std::array<int, 10> iarr2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::array<int, 10> iarr3 = {3};
 
-  std::cout << "*(fivec.begin()) = " << *(fivec.begin()) << std::endl;
-  std::cout << "*(svec.begin()) = " << *(dsvec.begin()) << std::endl;
+  for (auto iter = iarr3.begin(); iter != iarr3.end(); iter++) {
+    std::cout << *iter << std::endl;
+  }
 
   return 0;
 }
