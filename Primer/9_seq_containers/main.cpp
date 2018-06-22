f3cb1d15c597cd360f57985cd907f14c90f8a188
timtingwei <timtingwei@hotmail.com>
2018-06-22 19:11:29 +0800

    test reference of v.back()

--- Primer/9_seq_containers/main.cpp
+++ Primer/9_seq_containers/main.cpp
@@ -15,13 +15,21 @@ using std::vector; using std::list;
 using std::string;
 
 int main() {
-  vector<int> c{2, 3, 4, 9, -2, -3};
-  if (!c.empty()) {
-    auto val1 = *c.cbegin(), val2 = c.front();
-    auto last = c.end();
+  vector<int> v{2, 3, 4, 9, -2, -3};
+  if (!v.empty()) {
+    v.front() = 42;
+    auto &r = v.back();
+    r = 1024;              // change the last value
+    std::cout << "v.back() = " << v.back() << std::endl;
 
-    auto val3 = *(--last), val4 = c.back();
-    std::cout << "val3 = " << val3 << " " << "val 4 = " << val4 << std::endl;
+    auto nr = v.back();
+    // it's not reference to last value, can not change last one
+    nr = 88;
+    std::cout << "v.back() = " << v.back() << std::endl;
+
+    auto &r2 = v[v.size() - 1];   // change it
+    r2 = 7;
+    std::cout << "v.back() = " << v.back() << std::endl;
   }
   return 0;
 }
