daa20fd4eabeb9b2690a24ee273fdb9722588b23
timtingwei <timtingwei@hotmail.com>
2018-07-06 11:22:41 +0800

    add ignore file

--- Primer/9_seq_containers/main.cpp
+++ Primer/9_seq_containers/main.cpp
@@ -17,10 +17,14 @@ using std::string;
 
 int main() {
   Date();
-  Date("January 22, 888");
+  Date("January 22, 888xo");
   Date("12/03/1900");
   Date("Feb 2 1996");
   // January 1, 1900;  1/1/1900;  Jan 1 1900;
+  string s("abcs");
+  auto new_s = s.replace(0, 2, "abc");
+  std::cout << s << std::endl;
+  std::cout << new_s << std::endl;
 
   return 0;
 }
