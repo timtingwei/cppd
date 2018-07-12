47d7f89f0aa6a9401bec9936f17120c026d01e87
HankDaly <18868637597@163.com>
2018-07-12 15:32:57 +0800

    20180712

--- Primer/1_getting_started/CP5_ex1_11.h
+++ Primer/1_getting_started/CP5_ex1_11.h
@@ -4,7 +4,7 @@
 int foo_11()
 {   int v0,v1; 
     std::cout<< " Enter two integersThe former is greater than the latter "<<std::endl;
-    std::cin>>v0>>v1;
+    std::cin>>v0kjk>>v1;
     while(v1 <= v0)
     {
         std::cout<<v1<<std::endl;
---
+++ Primer/2_Variables_basic_types/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{    
+    unsigned u = 10, u2 = 42;
+    int i = 10 , i2 = 42;
+    std::cout<<u-i<<std::endl;
+
+    return 0;
+}
