2e0cbc94890beb887c74a7bc21e6ed4e2643f573
HankDaly <18868637597@163.com>
2018-07-16 22:50:51 +0800

    20180716

--- Primer/2_Variables_basic_types/test.cpp
+++ Primer/2_Variables_basic_types/test.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {    
-    int i = 0 , &r1 = i;
-    double d = 0,&r2 = d;
-    r1 = d;
-    std::cout<<r1<<std::endl;
+    const int v2 = 0;
+    int v1 = v2;
+    int *p1 = &v1, &r1 = v1;
+    const int *p2 = &v2;
+    p1 = p2;
+    std::cout<<p1<<std::endl;
     return 0;
 }
